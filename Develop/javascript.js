ec388aaeed5cc07f76c0458b85c3ce4a5f1d9d7f
JonkHunkle <dczerwinski@ymail.com>
2021-10-14 20:37:42 -0700

    fixed jQuery issues

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,19 +1,45 @@
-var timeBlock = $("#timeBlock");
-
-console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
-
-$(timeBlock).text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-
-console.log(timeBlock);
-
 //                  what to do?
 
-//~~add button to open planner and take moment in HH (2 digit hour format)
-//~~~~~~button propagates rows for hours in a business bay (9am-6pm)
-//~~~~~~~~~ hour in a column on left, text input in center, check off button on right
+//pull from local storage and propagate information
+//~~ rows for hours in a business bay (9am-6pm)
+//~~~~~ hour in a column on left, text input in center, save button on right\
+//~~~~~~~~~~save button alerts user the hour's input is saved in local storage
 
 //                   STYLING
 // current hour row background color is white
 //~~~if hour row is less than current hour --> background color is dark gray
 //~~~if hour row > current hour ---> background color is light gray
-//~~~ if check off button is clicked---> background is green
+
+//for loop going through business hours and making rows
+for (var i = 0; i < 9; i++) {
+  var hourEl = $("<div>");
+  hourEl.addClass("col");
+  hourEl.text(moment());
+  $("#timeBlock").append(hourEl);
+}
+//why doesnt this work an
+
+// function pull(){
+//     //pull info from local storage and create
+
+// }
+// function saveToLocal(){
+
+// }
+// function hourRows(){
+
+// }
+// function saveBtn(){
+
+// }
+
+// function checkHour(){
+
+// }
+
+console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
+// //$(timeBlock).text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+// console.log(timeBlock);
+
+// function addRow()
